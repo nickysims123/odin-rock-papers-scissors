04f53a3ae754ca4ddfab1dcde6ec8f81dc60ec22
Nicholas Sima <nick.sima@outlook.com>
2024-07-01 15:47:45 -0400

    Wrote getComputerChoice - using a helper function that returns 0,1,2, getComputerChoice then returns a move at random

---
+++ javascript.js
@@ -0,0 +1,23 @@
+function getRandomInt(num) {
+    return Math.floor(Math.random() * num);
+}
+
+function getComputerChoice() {
+    let num = getRandomInt(3);
+    if (num == 0) {
+        return "Rock";
+    }
+    else if (num == 1) {
+        return "Paper";
+    }
+    else {
+        return "Scissors";
+    }
+}
+
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
