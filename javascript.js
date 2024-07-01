5984d3954a1fe6a350c261f8aabf9b9b1e906462
Nicholas Sima <nick.sima@outlook.com>
2024-07-01 18:09:38 -0400

    Finished playRound function - increments scores correctly and decides winner thru series of if statements.

--- javascript.js
+++ javascript.js
@@ -22,14 +22,50 @@ function getHumanChoice() {
     if (choice != "Rock" && choice != "Paper" && choice != "Scissors") {
         choice = getHumanChoice();
     }
+    return choice;
 }
 
 let humanScore = 0;
 let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
+    console.log(humanChoice);
+    console.log(computerChoice);
     if (humanChoice == computerChoice) {
         console.log("It's a tie! You both picked ${humanChoice}, !");
+        return;
+    }
+
+    if (humanChoice == "Rock") {
+        if(computerChoice == "Paper") {
+            computerScore++;
+            console.log("You lose! Paper beats rock!")
+        }
+        else {
+            humanScore++;
+            console.log("You win! Rock beats scissors!")
+        }
+    }
+    else if (humanChoice == "Paper") {
+        if(computerChoice == "Scissors") {
+            computerScore++;
+            console.log("You lose! Scissors beats rock!")
+        }
+        else {
+            humanScore++;
+            console.log("You win! Paper beats rock!")
+        }
+    }
+
+    if (humanChoice == "Scissors") {
+        if(computerChoice == "Rock") {
+            computerScore++;
+            console.log("You lose! Rock beats scissors!")
+        }
+        else {
+            humanScore++;
+            console.log("You win! Scissors beats paper!")
+        }
     }
 }
 
@@ -37,3 +73,6 @@ const humanSelect = getHumanChoice();
 const computerSelect = getComputerChoice();
 
 playRound(humanSelect, computerSelect)
+
+console.log(humanScore);
+console.log(computerScore);
